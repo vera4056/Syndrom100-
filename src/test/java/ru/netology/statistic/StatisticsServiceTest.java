7a5d1d74185aab50cfb176e98a1fc4a09c5f3bdc
vera4056 <103599166+vera4056@users.noreply.github.com>
2022-07-07 19:47:41 +0300

    reformate code

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,8 +17,9 @@ public class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
     @Test
-    void findMaxInTheMiddle () {
+    void findMaxInTheMiddle() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 60, 8, 6, 11, 11, 12};
